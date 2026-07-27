2accfd328078d9d70c93fb9e3fd53a4c5eff2b2a
Sugimoto Asahi <sugimoto.asahi99@gmail.com>
2026-07-27 22:41:41 +0900

    chore: tidy up dev/release switch

--- config/neovim/init.lua
+++ config/neovim/init.lua
@@ -9,26 +9,30 @@ local function make_plugin_path(name)
 	return vim.fs.joinpath(base_path, name)
 end
 
-local function make_dev_path(name)
-	local base_path = vim.fs.normalize("~/.config/nvim-dev")
-	return vim.fs.joinpath(base_path, name)
-end
-
 local dev = os.getenv("NVIM_DEV")
-if dev and dev == "DEV" then
-	local dev_config_dir = vim.fs.normalize("~/.config/nvim-dev")
-	vim.opt.runtimepath:append(dev_config_dir)
-	local dev_config = require("config")
-	for name, dir in pairs(dev_config) do
-		local original_path = make_plugin_path(name)
-		local windows_original_path = original_path:gsub("/", "\\")
-		-- remove original paths
-		vim.opt.runtimepath:remove(original_path)
-		vim.opt.runtimepath:remove(windows_original_path)
+if dev then
+	if dev == "DEV" then
+		local dev_config_dir = vim.fs.normalize("~/.config/nvim-dev")
+		vim.opt.runtimepath:append(dev_config_dir)
+		local dev_config = require("config")
+		for name, dir in pairs(dev_config) do
+			local original_path = make_plugin_path(name)
+			local windows_original_path = original_path:gsub("/", "\\")
+			-- remove original paths
+			vim.opt.runtimepath:remove(original_path)
+			vim.opt.runtimepath:remove(windows_original_path)
 
-		-- add dev paths
-		vim.opt.runtimepath:append(dir)
+			-- add dev paths
+			vim.opt.runtimepath:append(dir)
+		end
+		vim.notify("Config: DEV")
+	elseif dev == "RELEASE" then
+		vim.notify("Config: RELEASE")
+	else
+		vim.notify('Invalid value for NVIM_DEV. Accepted values: "DEV", "RELEASE"', vim.log.levels.WARN)
 	end
+else
+	vim.notify("NVIM_DEV environment variable not set, defaulting to release config", vim.log.levels.INFO)
 end
 
 require("keymaps")
