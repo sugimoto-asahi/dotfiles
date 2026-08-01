fca11811a305681d15a746a70ea1455ddc9551d6
Sugimoto Asahi <sugimoto.asahi99@gmail.com>
2026-08-01 18:33:16 +0900

    fix: install uv only when uv is not presetn

--- dotfiles.ps1
+++ dotfiles.ps1
@@ -1,6 +1,6 @@
 $env:FORCE_COLOR = "1"
 # Install uv if not present
-if (Get-Command "uv" -ErrorAction SilentlyContinue) {
+if (!(Get-Command "uv" -ErrorAction SilentlyContinue)) {
     powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
 }
 
